b64652a4442a55bd1fd13e2afb356af3f9bd90e2
Digitl Alchemyst <129696730+Digitl-Alchemyst@users.noreply.github.com>
2024-11-04 12:16:51 -0600

    Sanity Base

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,11 +1,12 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable react/function-component-definition */
 'use client';
 
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+export default function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
